327329cf19db3d97ec4d371ae595c03a5d0facaa
Bruno <brunoagrizzi@gmail.com>
2019-09-01 21:01:58 -0300

    Modulo 03 Completed

--- modulo03/src/config/auth.js
+++ modulo03/src/config/auth.js
@@ -1,4 +1,4 @@
 export default {
-  secret: '73014b76c22e6b542c5b6d461d53e7c5',
+  secret: process.env.APP_SECRET,
   expiresIn: '7d',
 };
---
+++ modulo03/src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dns: 'https://a3ea903f02b74edaaa61b4f8d12f0d59@sentry.io/1548370',
+};
