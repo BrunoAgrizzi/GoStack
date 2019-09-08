668f21c01946c05b50adfb893c6b3efbea5de32f
Bruno <brunoagrizzi@gmail.com>
2019-09-07 21:00:30 -0300

    Desafio 03 Completed :tada: :heavy_check_mark:

---
+++ desafio03/src/database/index.js
@@ -0,0 +1,42 @@
+import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
+
+import databaseConfig from '../config/database';
+import User from '../app/models/User';
+import Meetup from '../app/models/Meetup';
+import File from '../app/models/File';
+import Subscription from '../app/models/Subscription';
+
+const models = [User, File, Meetup, Subscription];
+
+class Database {
+  constructor() {
+    this.connection = new Sequelize(databaseConfig);
+
+    this.mongo();
+
+    this.init();
+    this.associate();
+  }
+
+  init() {
+    models.forEach(model => model.init(this.connection));
+  }
+
+  associate() {
+    models.forEach(model => {
+      if (model.associate) {
+        model.associate(this.connection.models);
+      }
+    });
+  }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useFindAndModify: true,
+    });
+  }
+}
+
+export default new Database();
