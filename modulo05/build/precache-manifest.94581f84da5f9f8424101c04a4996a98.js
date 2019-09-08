78099be87c66b5f52fd0068e9da4395aa750d308
Bruno <brunoagrizzi@gmail.com>
2019-09-08 02:22:51 -0300

    Modulo 05 :construction:

---
+++ modulo05/build/precache-manifest.94581f84da5f9f8424101c04a4996a98.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ccd010f9ae5262995a0f8874a03c65c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "acb213503d54e253b0f9",
+    "url": "/static/js/2.5c87d150.chunk.js"
+  },
+  {
+    "revision": "c3c24010b4c3b71b38aa",
+    "url": "/static/js/main.8d34f731.chunk.js"
+  },
+  {
+    "revision": "c36331ff2101e375efd0",
+    "url": "/static/js/runtime~main.d57b7d8b.js"
+  }
+]);
